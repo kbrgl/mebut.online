db807ef297ca070cda7043a4bd7e848dafbaf203
Kabir Goel <kabirgoel.kg@gmail.com>
2022-06-13 21:49:17 -0700

    Add render plugin

--- .eleventy.js
+++ .eleventy.js
@@ -1,8 +1,11 @@
 require("dotenv").config()
 const markdownIt = require("markdown-it")
 const markdownItFootnote = require("markdown-it-footnote")
+const { EleventyRenderPlugin } = require("@11ty/eleventy")
 
 module.exports = function (eleventyConfig) {
+  eleventyConfig.addPlugin(EleventyRenderPlugin)
+
   eleventyConfig.addPassthroughCopy({
     public: "./",
   })
